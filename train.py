bcae0382f9b9c51f417fe5b3971928222e61a0d2
doa-ddaram <sqoml123@naver.com>
2025-02-25 13:16:28 +0000

    250225

--- train.py
+++ train.py
@@ -1,40 +1,45 @@
 import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from torchvision.datasets import MNIST
-from utils.model.cnn import CNN
-from utils.model.snn import SNN
+from torchvision.models import vgg16
+from torchvision.datasets import MNIST, CIFAR10
+from utils.model.cnn import CNN,VGG, make_layers
+from utils.model.snn import SNN, SNN_VGG
 from utils.model.stdp import SNN_STDP
 from tqdm.auto import tqdm
 import argparse
-from utils.spikingjelly.spikingjelly.activation_based import functional, layer, learning
+from utils.spikingjelly.spikingjelly.activation_based import functional, layer, learning, encoding, neuron, surrogate
+from utils.spikingjelly.spikingjelly.activation_based.model import spiking_vgg
 from utils.Adversarial.nes import *
-from utils.Adversarial.adversial_image import save_image
-from typing import Tuple
+from utils.Adversarial.adversial_image import generate_adversial_image, save_image
+from typing import Tuple, Callable
 from torch.autograd import Variable
-import numpy as np
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--net', type = str, help = "Input Model type (CNN or SNN or STDP)")
 parser.add_argument('-t', type = int, help = 'training time step')
 parser.add_argument('--seed', type = int, help = 'fixed random seed')
+parser.add_argument('--dset', type = str, help = 'input dataset.')
+parser.add_argument('--attack_status', type = bool, help = 'attack or not, type = bool')
 args = parser.parse_args()
 
 def train_CNN(
     net : nn.Module,  
-    data_loader : DataLoader,
-    loss_fn,
+    data_loader : DataLoader[tuple[th.Tensor, th.Tensor]],
+    loss_fn : Callable[[th.Tensor,th.Tensor], th.Tensor],
     num_epochs : int = 10,
-    length : int = 60000
           ) -> None : 
+    if th.cuda.device_count() > 1 :
+        net = nn.DataParallel(net)
     for epoch in range(num_epochs):
         total_loss, total_acc = 0, 0
         net.train()
+        length = 0
         for i, (data, target) in tqdm(enumerate(iter(data_loader))):
-            data = data.to(device)
-            target = target.to(device)
+            data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
             y_hat = net(data)
             loss = loss_fn(y_hat, target)
@@ -43,111 +48,164 @@ def train_CNN(
             total_loss += loss.item()
             pred_target = y_hat.argmax(1)
             total_acc += (pred_target == target).sum()
+            length += len(target)
         loss = total_loss / length
-        acc = (total_acc / length) * 100 
+        acc = (total_acc / length) * 100
         print(f'{epoch + 1} epoch\'s of Loss : {loss}, accuracy rate : {acc}')
-        
+        if epoch % 10 == 0:
+            test_loss, test_acc = test_CNN(net, test_loader, loss_fn = Loss_function)
+            print(f'acc of {epoch+1} : {test_acc}, and loss of {epoch+1} : {test_loss}')
+                    
 def test_CNN(
     net : nn.Module,
-    data_loader : DataLoader, 
-    loss_fn,
-    length : int = 10000,
+    data_loader : DataLoader[tuple[th.Tensor, th.Tensor]], 
+    loss_fn : Callable[[th.Tensor, th.Tensor], th.Tensor],
              ) -> Tuple[float, float]: 
-    total_loss, total_acc = 0, 0
+    total_acc_clean, total_acc_adv = 0, 0
+    total_loss_clean, total_loss_adv = 0, 0
     net.eval()
+    length = 0
     with th.no_grad():
         for i, (data, target) in tqdm(enumerate(iter(data_loader))):
             data, target = data.to(device), target.to(device)
-            adv_imgs = nes(net, data, target, iteration= 5, sample_size=10)
-            y_hat = net(adv_imgs)
+            y_hat_clean= net(data)
+            loss_clean = loss_fn(y_hat_clean, target)
+            total_loss_clean += loss_clean.item()
+            pred_target_clean = y_hat_clean.argmax(1)
+            total_acc_clean += (pred_target_clean == target).sum().item()
+            length += len(target)
+        total_loss_clean /= length
+        total_acc_clean = total_acc_clean / length * 100
+        loss, acc = (total_loss_clean) / length, (total_acc_clean / length) * 100
+        print(total_loss_clean, total_acc_clean)
+    with th.no_grad():
+        for i, (data, target) in tqdm(enumerate(iter(data_loader))):
+            data, target = data.to(device), target.to(device)
+            adv_imgs = generate_adversial_image(net, data, target)
+            y_hat_adv = net(adv_imgs)
             save_image(data,adv_imgs, './images/comparison_image_cnn.png', target)
-            loss = loss_fn(y_hat, target)
-            total_loss += loss.item()
-            pred_target = y_hat.argmax(1)
-            total_acc += (pred_target == target).sum()
-        loss = total_loss / length
-        acc = (total_acc / length) * 100
+            loss_adv = loss_fn(y_hat_adv, target)
+            total_loss_adv += loss_adv.item()
+            pred_target_adv = y_hat_adv.argmax(1)
+            total_acc_adv += (pred_target_adv == target).sum().item()
+        total_loss_clean /= length
+        total_acc_clean = total_acc_clean / length * 100
+        total_loss_adv /= length
+        total_acc_adv = total_acc_adv / length * 100
+        print(total_loss_adv, total_acc_adv)
+        loss, acc = total_loss_clean, total_acc_clean
     return loss, acc
 
 def train_SNN(
     net : nn.Module,  
-    data_loader : DataLoader,
-    loss_fn,
-    num_epochs : int = 10,
-    length : int = 60000
+    data_loader : DataLoader[tuple[th.Tensor, th.Tensor]],
+    num_epochs : int = 10
           ) -> None : 
+    T = 20
+    encoder = encoding.PoissonEncoder()
     for epoch in range(num_epochs):
         total_loss, total_acc = 0, 0
         net.train()
+        length = 0
         for i, (data, target) in tqdm(enumerate(iter(data_loader))):
-            data = data.to(device)
-            target = target.to(device)
+            data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
-            y_hat = net(data)
-            loss = loss_fn(y_hat, target)
+            target_onehot = F.one_hot(target, 10).float()
+            y_hat = 0.0
+            for _ in range(T):
+                encode = encoder(data)
+                y_hat += net(encode)
+            y_hat /= T
+            loss = F.mse_loss(y_hat, target_onehot)
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
             pred_target = y_hat.argmax(1)
             total_acc += (pred_target == target).sum()
+            length += len(target)
             functional.reset_net(net)
         loss = total_loss / length
         acc = (total_acc / length) * 100 
         print(f'{epoch + 1} epoch\'s of Loss : {loss}, accuracy rate : {acc}')
-
+        
 def test_SNN(
     net : nn.Module,
-    data_loader : DataLoader, 
-    loss_fn,
-    length : int = 10000,
+    data_loader : DataLoader
              ) -> Tuple[float, float]: 
+    encoder = encoding.PoissonEncoder()
+    T = 20
+    total_acc_clean, total_acc_adv = 0, 0
+    total_loss_clean, total_loss_adv = 0, 0
     net.eval()
-    total_loss, total_acc = 0, 0
+    length = 0
     with th.no_grad():
         for i, (data, target) in tqdm(enumerate(iter(data_loader))):
             data, target = data.to(device), target.to(device)
-            adv_imgs = nes(net, data, target, iteration= 5, sample_size=10)
-            y_hat = net(adv_imgs)
-            save_image(data,adv_imgs, './images/comparison_image_snn.png', target)
-            loss = loss_fn(y_hat, target)
-            total_loss += loss.item()
-            pred_target = y_hat.argmax(1)
-            total_acc += (pred_target == target).sum()
-            functional.reset_net(net)
-        loss = total_loss / length
-        acc = (total_acc / length) * 100
+            target_onehot = F.one_hot(target, 10).float()
+            y_hat_clean = 0.0
+            for _ in range(T):
+                encode_clean = encoder(data)
+                y_hat_clean += net(encode_clean)
+            y_hat_clean /= T
+            loss_clean = F.mse_loss(y_hat_clean, target_onehot)
+            total_loss_clean += loss_clean.item()
+            pred_target_clean = y_hat_clean.argmax(1)
+            total_acc_clean += (pred_target_clean == target).sum().item()
+            length += len(target)
+        total_loss_clean /= length
+        total_acc_clean = total_acc_clean / length * 100
+        print(total_loss_clean, total_acc_clean)
+    with th.no_grad():
+        for i, (data, target) in tqdm(enumerate(iter(data_loader))):
+            data, target = data.to(device), target.to(device)
+            adv_imgs = nes(net, data, target, iteration = 10, sample_size= 20)
+            target_onehot = F.one_hot(target, 10).float()
+            y_hat_adv = 0.0
+            for _ in range(T):
+                encode_adv = encoder(adv_imgs)
+                y_hat_adv += net(encode_adv)
+            y_hat_adv /= T
+            save_image(data,adv_imgs, './images/comparison_image_cnn.png', target)
+            loss_adv = F.mse_loss(y_hat_adv, target_onehot)
+            total_loss_adv += loss_adv.item()
+            pred_target_adv = y_hat_adv.argmax(1)
+            total_acc_adv += (pred_target_adv == target).sum().item()
+        total_loss_adv /= length
+        total_acc_adv = total_acc_adv / length * 100
+        print(total_loss_adv, total_acc_adv)
+        loss, acc = total_loss_clean, total_acc_clean
     return loss, acc
+
 
 def train_STDP(
     net : nn.Module,  
     data_loader : DataLoader,
     loss_fn,
-    num_epochs : int = 10,
-    length : int = 60000
-          ) -> None :
-    if th.cuda.device_count() > 1 :
-        net = nn.DataParallel(net)
+    num_epochs : int = 10
+        ) -> None :
+
     instances_stdp = ( layer.Linear,
                         layer.Conv1d,
                         layer.BatchNorm2d,
                         layer.Conv2d,
                         layer.MaxPool2d,)
     stdp_learners = []
-
+    net = net.to(device)
     step_mode = 'm'
     tau_pre = 2.
     tau_post = 10.
     def f_weight(x):
         return th.clamp(x, -1, 1.)
-    
+    if th.cuda.device_count() > 1 :
+        net = nn.DataParallel(net)
     functional.set_step_mode(net, 'm')
-
     for epoch in range(num_epochs):
         total_loss, total_acc = 0, 0
+        length = 0
         for i, (data, target) in tqdm(enumerate(iter(data_loader))):
             data, target = data.to(device), target.to(device)    
             data, target = Variable(data), Variable(target)
-            data = data.unsqueeze(0).repeat(8, 1, 1, 1, 1)
+            data = data.unsqueeze(0).repeat(20, 1, 1, 1, 1)
             for i, layers in enumerate(net.children()):
                 if isinstance(layers, instances_stdp):
                     if i + 2 < len(list(net.children())) :
@@ -193,36 +251,48 @@ def train_STDP(
             total_loss += loss.item()
             pred_target = y_hat.argmax(1)
             total_acc += (pred_target == target).sum()
+            length += len(target)
         loss = total_loss / length
         acc = (total_acc / length) * 100 
-        
-        print(f'{epoch} epoch\'s of Loss : {loss}, accuracy rate : {acc}')
+        print(f'{epoch + 1} epoch\'s of Loss : {loss}, accuracy rate : {acc}')
 
 def test_STDP(
     net : nn.Module,
     data_loader : DataLoader, 
-    loss_fn,
-    length : int = 10000,
+    loss_fn
              ) -> Tuple[float, float]: 
     net.eval()
     net = net.to(device)
     if th.cuda.device_count() > 1 :
         net = nn.DataParallel(net)
-    total_loss, total_acc = 0, 0
+    total_acc_clean, total_acc_adv = 0, 0
+    total_loss_clean, total_loss_adv = 0, 0
+    length = 0
     for i, (data, target) in tqdm (enumerate(iter(data_loader))):
         data, target = data.to(device), target.to(device)
-        data, target = Variable(data), Variable(target) # Yet, not use adversarial attack. because of unique nature of stdp. 
-        data = data.unsqueeze(0).repeat(8, 1, 1, 1, 1)
-        y_hat = net(data).mean(0)
-        loss = loss_fn(y_hat, target)
-        total_loss += loss.item()
-        pred_target = y_hat.argmax(1)
-        total_acc += (pred_target == target).sum()
+        data = data.unsqueeze(0).repeat(20, 1, 1, 1, 1)
+        adv_imgs = nes_stdp(net, data, target, iteration= 5, sample_size=10)
+        data, target = Variable(data), Variable(target) 
+        y_hat_clean = net(data).mean(0)
+        y_hat_adv = net(adv_imgs).mean(0)
+        loss_clean = loss_fn(y_hat_clean, target)
+        loss_adv = loss_fn(y_hat_adv, target)
+        total_loss_clean += loss_clean.item()
+        total_loss_adv += loss_adv.item()
+        pred_target_clean = y_hat_clean.argmax(1)
+        pred_target_adv = y_hat_adv.argmax(1)
+        total_acc_clean += (pred_target_clean == target).sum().item()
+        total_acc_adv += (pred_target_adv == target).sum().item()
         functional.reset_net(net)
-    loss = total_loss / length
-    acc = (total_acc / length) * 100
+        length += len(target)
+    total_loss_clean /= length
+    total_acc_clean = total_acc_clean / length * 100
+    print(total_loss_clean, total_acc_clean)
+    total_loss_adv /= length
+    total_acc_adv = total_acc_adv / length * 100
+    print(total_loss_adv, total_acc_adv)
+    loss, acc = total_loss_clean, total_acc_clean
     return loss, acc
-
 if __name__ == "__main__":
     num_epochs = args.t
     batch_size = 32
@@ -230,68 +300,92 @@ if __name__ == "__main__":
     learning_rate = 1e-2
     seed = args.seed
     
-    np.random.seed(seed)
+    th.manual_seed(seed)
+    th.cuda.manual_seed_all(seed)
+    
     device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
     
-    MNIST_train = MNIST(root = './data', download = True, train = True, transform = transforms.ToTensor())
-    MNIST_test = MNIST(root = './data', download = True, train = False, transform = transforms.ToTensor())
-    train_len = len(MNIST_train)
-    test_len = len(MNIST_test)
-    train_loader = DataLoader(
-        MNIST_train, batch_size = batch_size, shuffle = True, num_workers = num_workers
-    )
-    test_loader = DataLoader(
-        MNIST_test, batch_size = batch_size, shuffle = False, num_workers = num_workers
-    )
+    if args.dset == 'MNIST' :
+        MNIST_train = MNIST(root = './data', download = True, train = True, transform = transforms.ToTensor())
+        MNIST_test = MNIST(root = './data', download = True, train = False, transform = transforms.ToTensor())
+        
+        train_loader = DataLoader(
+            MNIST_train, batch_size = batch_size, shuffle = True, num_workers = num_workers
+        )
+        test_loader = DataLoader(
+            MNIST_test, batch_size = batch_size, shuffle = False, num_workers = num_workers
+        )
+    
+    else:
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding = 4),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    
+        
+        CIFAR10_train = CIFAR10(root = './data/CIFAR10', download= True, train= True, transform= transform)
+        CIFAR10_test = CIFAR10(root= './data/CIFAR10', download=True, train = False, transform=transform)
+        
+        train_loader = DataLoader(CIFAR10_train, batch_size = batch_size, shuffle=True, num_workers= num_workers)
+        test_loader = DataLoader(CIFAR10_test, batch_size = batch_size, shuffle=False, num_workers= num_workers)
+    
     Loss_function= nn.CrossEntropyLoss().to(device)
     if args.net == 'CNN':
-        net = CNN().to(device)
+        if args.dset == 'MNIST':
+            net = CNN().to(device)
+        else:
+            net = vgg16().to(device)
         optimizer = th.optim.Adam(net.parameters(), lr = learning_rate)
         train_CNN(
             net = net,
             num_epochs = num_epochs,
             data_loader= train_loader,
-            loss_fn = Loss_function,
-            length = train_len
-            )
-        test_loss, test_acc = test_CNN(
-            net = net,
-            data_loader=test_loader,
-            loss_fn = Loss_function,
-            length = test_len
+            loss_fn = Loss_function
             )
     elif args.net == 'SNN':
-        net = SNN().to(device)
+        if args.dset == 'MNIST':
+            net = SNN().to(device)
+        else:
+            net = spiking_vgg.spiking_vgg16(spiking_neuron = neuron.LIFNode, surrogate_function = surrogate.ATan()).to(device)
         optimizer = th.optim.Adam(net.parameters(), lr = learning_rate)
         train_SNN(
             net = net,
             num_epochs = num_epochs,
-            data_loader= train_loader,
-            loss_fn = Loss_function, 
-            length = train_len
+            data_loader= train_loader
             )
-        test_loss, test_acc = test_SNN(
-            net = net,
-            data_loader = test_loader,
-            loss_fn = Loss_function,
-            length = test_len
-            )
+        test_loss, test_acc = test_SNN(net, test_loader)
     else:
-        net = SNN_STDP().to(device)
+        if args.dset == 'MNIST':
+            net = SNN_STDP().to(device)
+        else :
+            net = spiking_vgg.spiking_vgg16(spiking_neuron = neuron.LIFNode, surrogate_function = surrogate.ATan()).to(device)
         optimizer = th.optim.Adam(net.parameters(), lr = learning_rate)
+        stdp_learners = []
+
+        step_mode = 'm'
+        tau_pre = 2.
+        tau_post = 10.
+        def f_weight(x):
+            return th.clamp(x, -1, 1.)
+
+        functional.set_step_mode(net, 'm')
+        '''
+        In SNN and CNN, Can put a test process inside a train process,
+        but STDP still doesn't this work, so temporarily devide train process and test process. 
+        '''
         train_STDP(
             net = net,
             num_epochs= num_epochs,
             data_loader = train_loader,
-            loss_fn = Loss_function,
-            length = train_len
+            loss_fn = Loss_function
             )
         test_loss, test_acc = test_STDP(
             net = net,
             data_loader = test_loader,
-            loss_fn = Loss_function,
-            length = test_len
+            loss_fn = Loss_function
         )
     # total_params = sum(p.numel() for p in net.parameters())
     # print(total_params)    
-    print(f'test loss = {test_loss}, and test accuracy = {test_acc}')
+
--- utils/model/cnn.py
+++ utils/model/cnn.py
@@ -1,24 +1,62 @@
 import torch as th
 import torch.nn as nn
+from typing import Callable,List
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
-        )
-        self.linear1 = nn.Linear(in_features=7 * 7 * 64, out_features=10, bias=False)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 3, padding = 1)
+        self.pool = nn.MaxPool2d(kernel_size= 2, stride= 2)
+        self.relu = nn.ReLU()
+        self.linear = nn.Linear(in_features=7 * 7 * 64, out_features=10, bias=False)
         
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
+    def forward(self, x: th.Tensor) -> th.Tensor:
+        '''
+        Args:
+            x (th.Tensor) : Input tensor, shape (batch_size, 1, 28, 28)
+            
+        Returns:
+            th.Tensor: Output tensor, shape (batch_size, 10)
+        '''
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
         x = x.view(x.size(0), -1 )
-        x = self.linear1(x)
+        x = self.linear(x)
+        return x
+    
+def make_layers(cfg : List, batch_norm : bool = False) : 
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers.append(nn.MaxPool2d(kernel_size= 2, stride= 2))
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size= 3, padding = 1)
+            if batch_norm :
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+            
+    return nn.Sequential(*layers)
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes = 10):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(512,512),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(512, num_classes),
+        )
+    def forward(self, x) :
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
         return x
