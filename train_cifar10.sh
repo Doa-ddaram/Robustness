785b168c8dfb9ec87f73f83d616cc09a418e47a7
doa-ddaram <sqoml123@naver.com>
2025-07-30 09:04:50 +0900

    Update README.md, Rename requirements, and Update script file

--- train_cifar10.sh
+++ train_cifar10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-python main.py --net CNN -t 100 --seed 0 --dset CIFAR10 --attack --epsilon 0.009 --learning_rate 1e-4 --batch_size 32 --timestep 20 --save;
-python main.py --net SNN -t 100 --seed 0 --dset CIFAR10 --attack --epsilon 0.009 --learning_rate 1e-4 --batch_size 32 --timestep 20 --save;
-python main.py --net STDP -t 100 --seed 0 --dset CIFAR10 --attack --epsilon 0.009 --learning_rate 1e-4 --batch_size 32 --timestep 20 --save;
+python main.py --net CNN -t 50 --seed 0 --dset CIFAR10 --attack --epsilon 0.018 --learning_rate 0.05 --batch_size 64 --timestep 20 --save;
+python main.py --net SNN -t 50 --seed 0 --dset CIFAR10 --attack --epsilon 0.018 --learning_rate 0.05 --batch_size 64 --timestep 20 --save;
+python main.py --net STDP -t 50 --seed 0 --dset CIFAR10 --attack --epsilon 0.018 --learning_rate 0.05 --batch_size 64 --timestep 20 --save;
